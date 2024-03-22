4880481985f678c31a5119900b60ceb7267dc24c
Noyaa5580 <timofeybarya2005@gmail.com>
2024-03-22 12:14:36 +0300

    catalog

---
+++ js/catalog.js
@@ -0,0 +1,82 @@
+let button1 = document.getElementById('button1');
+let button2 = document.getElementById('button2');
+let button3 = document.getElementById('button3');
+let button4 = document.getElementById('button4');
+let button5 = document.getElementById('button5');
+let button6 = document.getElementById('button6');
+let button7 = document.getElementById('button7');
+let button8 = document.getElementById('button8');
+let button9 = document.getElementById('button9');
+
+function mouseover1(){
+    button1.style.visibility = 'visible';
+    button1.style.opacity = '1';
+}
+function mouseleave1(){
+    button1.style.visibility = 'hidden';
+    button1.style.opacity = '0';
+}
+function mouseover2(){
+    button2.style.visibility = 'visible';
+    button2.style.opacity = '1';
+}
+function mouseleave2(){
+    button2.style.visibility = 'hidden';
+    button2.style.opacity = '0';
+}
+function mouseover3(){
+    button3.style.visibility = 'visible';
+    button3.style.opacity = '1';
+}
+function mouseleave3(){
+    button3.style.visibility = 'hidden';
+    button3.style.opacity = '0';
+}
+function mouseover4(){
+    button4.style.visibility = 'visible';
+    button4.style.opacity = '1';
+}
+function mouseleave4(){
+    button4.style.visibility = 'hidden';
+    button4.style.opacity = '0';
+}
+function mouseover5(){
+    button5.style.visibility = 'visible';
+    button5.style.opacity = '1';
+}
+function mouseleave5(){
+    button5.style.visibility = 'hidden';
+    button5.style.opacity = '0';
+}
+function mouseover6(){
+    button6.style.visibility = 'visible';
+    button6.style.opacity = '1';
+}
+function mouseleave6(){
+    button6.style.visibility = 'hidden';
+    button6.style.opacity = '0';
+}
+function mouseover7(){
+    button7.style.visibility = 'visible';
+    button7.style.opacity = '1';
+}
+function mouseleave7(){
+    button7.style.visibility = 'hidden';
+    button7.style.opacity = '0';
+}
+function mouseover8(){
+    button8.style.visibility = 'visible';
+    button8.style.opacity = '1';
+}
+function mouseleave8(){
+    button8.style.visibility = 'hidden';
+    button8.style.opacity = '0';
+}
+function mouseover9(){
+    button9.style.visibility = 'visible';
+    button9.style.opacity = '1';
+}
+function mouseleave9(){
+    button9.style.visibility = 'hidden';
+    button9.style.opacity = '0';
+}
